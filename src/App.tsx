b7054b10d8faa60cdca720e3da186a6c0aea1ac3
JamesOkemwa <okejames55@gmail.com>
2021-09-09 14:41:47 +0300

    Project Logic, Handling State, Handling Props, Handling Fuctions, Handling Events

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List'
+import AddToList from './components/AddToList';
+
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
 
 function App() {
+
+  const [ people, setPeople ] = useState<IState["people"]>([
+    {
+      name: "James",
+      age: 25,
+      url: "https://cdn.pixabay.com/photo/2021/03/03/14/55/rhino-6065480__480.jpg",
+      note: "Full Stack Developer"
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Project Team Members</h1>
+      <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
